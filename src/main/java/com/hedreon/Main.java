160777ec8124ed76930af7024f398db4bd0870c0
Hedreon <hedreon@e.email>
2023-02-25 23:44:48 +0000

    new message
    just a boiler commit used to get this stupid automatic merge working

--- src/main/java/com/hedreon/Main.java
+++ src/main/java/com/hedreon/Main.java
@@ -2,7 +2,7 @@ package com.hedreon;
 
 public class Main {
     public static void main(String[] args) {
-        // Warns user that the project is still in its initial stages.
-        System.out.println("[WARNING]: Project Hedronic Launcher is still in it's initial stages, so expect nothing for a short while!");
+        // Warns user that the project is still in its initial stages
+        System.out.println("[WARNING]: Hedronic Launcher is still in its initial stages, so dont expect anything for a short while!");
     }
 }
