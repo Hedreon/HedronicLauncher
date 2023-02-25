7b64396187e0d7f328809ce9010c6536be83348d
Hedreon <hedreon@e.email>
2023-02-25 23:46:54 +0000

    !

--- src/main/java/com/hedreon/Main.java
+++ src/main/java/com/hedreon/Main.java
@@ -3,6 +3,6 @@ package com.hedreon;
 public class Main {
     public static void main(String[] args) {
         // Warns user that the project is still in its initial stages
-        System.out.println("[WARNING]: Hedronic Launcher is still in its initial stages, so dont expect anything for a short while!");
+        System.out.println("[WARNING]: Hedronic Launcher is still in its initial stages, so dont expect anything for a short while!!");
     }
 }
